691c61a4ff7dd5880ee802bf47ac89ccb5d0eb3b
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2012-08-07 16:59:15 +0900

    add quirks for _abc_ on cjk

--- prep-cjk.py
+++ prep-cjk.py
@@ -6,17 +6,33 @@ import re
 import unicodedata
 
 
-def joinCjkLine( src ):
+def joinCjkLine( text ):
+	eaw = unicodedata.east_asian_width
+	
 	def subst( m ):
-		if(
-			unicodedata.east_asian_width( m.group( 1 ) ) == "W" and
-			unicodedata.east_asian_width( m.group( 2 ) ) == "W"
-		):
+		if eaw( m.group( 1 ) ) == "W" and eaw( m.group( 2 ) ) == "W":
 			return m.group( 1 ) + m.group( 2 )
 		else:
 			return m.group( 0 )
 
-	return re.sub( r"([\S])[^\S\n]*\n[^\S\n]*([\S])", subst, src )
+	text = re.sub( r"([\S])[^\S\n]*\n[^\S\n]*([\S])", subst, text )
+
+	def subst( m ):
+		if eaw( m.group( 1 )[:+1] ) == "W":
+			prefix = "_"
+		else:
+			prefix = " _"
+
+		if eaw( m.group( 1 )[-1:] ) == "W":
+			suffix = "_"
+		else:
+			suffix = "_ "
+
+		return prefix + m.group( 1 ) + suffix
+
+	text = re.sub( r"[\s]+_([^_]+)_[\s]+", subst, text )
+
+	return text
 
 
 if __name__ == "__main__":
